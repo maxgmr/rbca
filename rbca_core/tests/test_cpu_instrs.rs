3efd5670b8bfef6743e5467b2b96b71191f9683a
maxgmr <mxzmxz120@gmail.com>
2024-09-06 20:28:15 -0700

    impl test logging

--- rbca_core/tests/test_cpu_instrs.rs
+++ rbca_core/tests/test_cpu_instrs.rs
@@ -1,6 +1,6 @@
 #![cfg(test)]
 
-use std::time::Instant;
+use std::{fs::OpenOptions, io::prelude::*, time::Instant};
 
 mod common;
 
@@ -14,21 +14,40 @@ use rbca_core::{
 use text_io::read;
 
 fn test_common(rom_name: &str) {
-    // const BREAKPOINTS: bool = true;
-    const BREAKPOINTS: bool = false;
+    const BREAKPOINTS: bool = true;
+    // const BREAKPOINTS: bool = false;
     const STEP_FORWARD: bool = true;
     // const STEP_FORWARD: bool = false;
     // const SLOW: bool = true;
     const SLOW: bool = false;
     const WAIT_MS: u64 = 50;
+    const LOG: bool = true;
+    // const LOG: bool = false;
 
     #[allow(unused_variables)]
-    fn is_breakpoint(cpu: &Cpu, last_state: &CpuState, total_cycles: u128) -> bool {
-        cpu.pc > 0x020F
-        // cpu.pc == 0x0064
+    fn is_breakpoint(
+        cpu: &Cpu,
+        last_state: &CpuState,
+        total_cycles: u128,
+        total_steps: u128,
+    ) -> bool {
+        total_steps == 2496
+        // cpu.pc == 0xDEF8
         // cpu.regs.get_reg(D) != last_state.regs.get_reg(D)
         // (cpu.regs.get_flag(RegFlag::C) != last_state.regs.get_flag(RegFlag::C)) || (cpu.pc > 0xC000)
     }
+
+    #[allow(unused_assignments)]
+    let mut log_file = OpenOptions::new()
+        .write(true)
+        .create(true)
+        .truncate(LOG)
+        .open(format!("{}_LOG", &rom_name[0..=1]))
+        .unwrap();
+    log_file = OpenOptions::new()
+        .append(true)
+        .open(format!("{}_LOG", &rom_name[0..=1]))
+        .unwrap();
 
     let mut cpu = Cpu::new();
     cpu.mem_bus.load_cart(
@@ -53,6 +72,8 @@ fn test_common(rom_name: &str) {
 
     cpu.pc = 0x0100;
     cpu.sp = 0xFFFE;
+    cpu.mem_bus
+        .write_byte(0xFF40, cpu.mem_bus.read_byte(0xFF40) | 0b1000_0000);
     cpu.regs.set_reg(A, 0x01);
     cpu.regs.set_reg(B, 0x00);
     cpu.regs.set_reg(C, 0x13);
@@ -67,13 +88,28 @@ fn test_common(rom_name: &str) {
 
     let mut last_state = CpuState::save_state(&cpu);
     let mut last_break: bool = false;
+    let mut last_step_forward: bool = false;
 
     let mut t_cycles = 0;
     let mut total_cycles: u128 = 0;
+    let mut total_steps: u128 = 0;
 
     let mut blargg_out = String::new();
 
+    let mut log_queue: String = String::with_capacity(18000 * 200);
+
     loop {
+        total_steps += 1;
+        // log file
+        // gameboy-doctor version
+        if LOG {
+            log_queue.push_str(&format!("A:{:02X} F:{:02X} B:{:02X} C:{:02X} D:{:02X} E:{:02X} H:{:02X} L:{:02X} SP:{:04X} PC:{:04X} PCMEM:{:02X},{:02X},{:02X},{:02X}\n", cpu.regs.get_reg(A), cpu.regs.get_flag_byte(), cpu.regs.get_reg(B), cpu.regs.get_reg(C), cpu.regs.get_reg(D), cpu.regs.get_reg(E), cpu.regs.get_reg(H), cpu.regs.get_reg(L), cpu.sp, cpu.pc, cpu.mem_bus.read_byte(cpu.pc), cpu.mem_bus.read_byte(cpu.pc + 1), cpu.mem_bus.read_byte(cpu.pc + 2), cpu.mem_bus.read_byte(cpu.pc + 3)));
+        }
+        // logdbg version
+        // if LOG {
+        //     log_queue.push_str(&format!("A: {:02X} F: {:02X} B: {:02X} C: {:02X} D: {:02X} E: {:02X} H: {:02X} L: {:02X} SP: {:04X} PC: 00:{:04X} ({:02X} {:02X} {:02X} {:02X})\n", cpu.regs.get_reg(A), cpu.regs.get_flag_byte(), cpu.regs.get_reg(B), cpu.regs.get_reg(C), cpu.regs.get_reg(D), cpu.regs.get_reg(E), cpu.regs.get_reg(H), cpu.regs.get_reg(L), cpu.sp, cpu.pc, cpu.mem_bus.read_byte(cpu.pc), cpu.mem_bus.read_byte(cpu.pc + 1), cpu.mem_bus.read_byte(cpu.pc + 2), cpu.mem_bus.read_byte(cpu.pc + 3)));
+        // }
+
         last_state.update(&cpu);
         let t_start = Instant::now();
         let cycles = cpu.cycle();
@@ -95,21 +131,33 @@ fn test_common(rom_name: &str) {
         }
 
         // breakpoints
-        if BREAKPOINTS && is_breakpoint(&cpu, &last_state, total_cycles) {
-            last_break = true;
+        if BREAKPOINTS && last_break {
+            last_break = false;
+            last_step_forward = true;
+            println!("{total_steps}");
             println!(" - BREAK - ");
             let _: String = read!();
-        } else if BREAKPOINTS && STEP_FORWARD && last_break {
-            last_break = false;
+        } else if BREAKPOINTS && last_step_forward {
+            last_step_forward = false;
             println!(" - STEP FORWARD - ");
             let _: String = read!();
+        }
+        if BREAKPOINTS && is_breakpoint(&cpu, &last_state, total_cycles, total_steps + 1) {
+            last_break = true;
         }
 
         if SLOW {
             std::thread::sleep(std::time::Duration::from_millis(WAIT_MS));
-        } else if t_cycles >= 17476 {
+        }
+
+        if t_cycles >= 17476 {
             t_cycles %= 17476;
             while t_start.elapsed().as_millis() < 16 {}
+            if LOG {
+                if let Err(e) = write!(log_file, "{log_queue}") {
+                    eprintln!("ERROR: {}", e);
+                }
+            }
         }
     }
 }
