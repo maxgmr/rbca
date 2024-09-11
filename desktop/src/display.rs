de146c9b4cf770767e0ba1059d3fba4ae9cd8c45
maxgmr <mxzmxz120@gmail.com>
2024-09-11 14:19:21 -0700

    basic desktop frontend

--- desktop/src/display.rs
+++ desktop/src/display.rs
@@ -1,27 +1,35 @@
+use std::time::Instant;
+
 use color_eyre::eyre::{self, eyre};
-use rbca_core::{DISPLAY_HEIGHT, DISPLAY_WIDTH};
+use rbca_core::{Cpu, DISPLAY_HEIGHT, DISPLAY_WIDTH};
 use sdl2::{
     event::Event, keyboard::Keycode, pixels::Color, rect::Rect, render::Canvas, video::Window,
-    EventPump, Sdl, VideoSubsystem,
+    EventPump,
 };
+
+use super::INSTR_DEBUG;
 
 const SCALE: u32 = 5;
 
 const WINDOW_WIDTH: u32 = (DISPLAY_WIDTH as u32) * SCALE;
 const WINDOW_HEIGHT: u32 = (DISPLAY_HEIGHT as u32) * SCALE;
 
+// 0
+const WHITE: (u8, u8, u8) = (0xD7, 0xEC, 0xA1);
+// 1
+const LIGHT_GREY: (u8, u8, u8) = (0xA6, 0xBB, 0x72);
+// 2
+const DARK_GREY: (u8, u8, u8) = (0x6C, 0x7D, 0x41);
+// 3
+const BLACK: (u8, u8, u8) = (0x3B, 0x46, 0x20);
+
 pub struct Display {
-    sdl_context: Sdl,
-    video_subsystem: VideoSubsystem,
+    cpu: Cpu,
     canvas: Canvas<Window>,
     event_pump: EventPump,
-    // 3 bytes per pixel; RGB24 encoding
-    framebuffer: [u8; 3 * DISPLAY_WIDTH * DISPLAY_HEIGHT],
-    // 4 bytes per pixel; RGBA24 encoding
-    framebuffer_a: [u8; 4 * DISPLAY_WIDTH * DISPLAY_HEIGHT],
 }
 impl Display {
-    pub fn new() -> eyre::Result<Self> {
+    pub fn new(cpu: Cpu) -> eyre::Result<Self> {
         let sdl_context = match sdl2::init() {
             Ok(sdlc) => sdlc,
             Err(e) => return Err(eyre!(e)),
@@ -36,6 +44,7 @@ impl Display {
             .opengl()
             .build()?;
         let mut canvas = window.into_canvas().present_vsync().build()?;
+
         canvas.clear();
         canvas.present();
 
@@ -45,17 +54,18 @@ impl Display {
         };
 
         Ok(Self {
-            sdl_context,
-            video_subsystem,
+            cpu,
             canvas,
             event_pump,
-            framebuffer: [0x00; 3 * DISPLAY_WIDTH * DISPLAY_HEIGHT],
-            framebuffer_a: [0x00; 4 * DISPLAY_WIDTH * DISPLAY_HEIGHT],
         })
     }
 
     pub fn run(&mut self) -> eyre::Result<()> {
+        let mut cycles: u128 = 0;
         'main_loop: loop {
+            let start = Instant::now();
+
+            // Read key events
             for event in self.event_pump.poll_iter() {
                 match event {
                     Event::Quit { .. }
@@ -68,30 +78,44 @@ impl Display {
                     _ => {}
                 }
             }
-            self.draw_screen()?;
+
+            // Cycle CPU
+            cycles += self.cpu.cycle(INSTR_DEBUG) as u128;
+
+            // Approximately one frame
+            if cycles >= 70224 {
+                cycles %= 70224;
+                self.draw_screen()?;
+                // Wait until can start next frame
+                if start.elapsed().as_nanos() < 16_750_000 {}
+            }
         }
         Ok(())
     }
 
     fn draw_screen(&mut self) -> eyre::Result<()> {
         // Clear canvas
-        self.canvas.set_draw_color(Color::RGB(0xB8, 0xD7, 0x81));
+        self.canvas
+            .set_draw_color(Color::RGB(WHITE.0, WHITE.1, WHITE.2));
         self.canvas.clear();
 
-        // TODO get display here
-        // for (i, pixel) in self.display_arr.iter().enumerate() {
-        //     if *pixel {
-        //         // Convert index to 2D [x,y] position
-        //         let x = (i % DISPLAY_WIDTH) as u32;
-        //         let y = (i / DISPLAY_HEIGHT) as u32;
-        //
-        //         // Draw scaled-up rectangle @ [x,y]
-        //         let rect = Rect::new((x * SCALE) as i32, (y * SCALE) as i32, SCALE, SCALE);
-        //         self.canvas.fill_rect(rect).unwrap();
-        //     }
-        // }
-        self.canvas.present();
+        for (i, pixel) in self.cpu.get_pixels().iter().enumerate() {
+            let colour = match pixel {
+                0 => continue,
+                1 => LIGHT_GREY,
+                2 => DARK_GREY,
+                3 => BLACK,
+                _ => unreachable!("Illegal colour value."),
+            };
+            self.canvas
+                .set_draw_color(Color::RGB(colour.0, colour.1, colour.2));
+            let x = (i % DISPLAY_WIDTH) as u32;
+            let y = (i / DISPLAY_WIDTH) as u32;
+            let rect = Rect::new((x * SCALE) as i32, (y * SCALE) as i32, SCALE, SCALE);
+            self.canvas.fill_rect(rect).unwrap();
+        }
 
+        self.canvas.present();
         Ok(())
     }
 }
--- desktop/src/main.rs
+++ desktop/src/main.rs
@@ -2,23 +2,40 @@ use std::env;
 
 use color_eyre::eyre::{self, eyre};
 use rbca_core::Cpu;
+use text_io::read;
 
 mod display;
 
 use display::Display;
 
+const INSTR_DEBUG: bool = true;
+const USE_BOOT_ROM: bool = false;
+
 fn main() -> eyre::Result<()> {
     let args: Vec<_> = env::args().collect();
 
-    if args.len() != 2 {
+    if USE_BOOT_ROM && args.len() != 3 {
+        return Err(eyre!(
+            "Incorrect usage.\r\nUsage: cargo r path/to/boot/rom path/to/gb/rom"
+        ));
+    } else if !USE_BOOT_ROM && args.len() != 2 {
         return Err(eyre!("Incorrect usage.\r\nUsage: cargo r path/to/gb/rom"));
     }
 
     // Load ROM
-    let mut cpu = Cpu::new_cart(&args[1]);
+    let cpu = if USE_BOOT_ROM {
+        Cpu::new_boot_cart(&args[2], &args[1])
+    } else {
+        Cpu::new_cart(&args[1])
+    };
+
+    println!("{}", cpu.emu_info());
+    println!("Enter any text to start...");
+    let _: String = read!();
+    println!("-------");
 
     // Create desktop UI
-    let mut desktop = Display::new()?;
+    let mut desktop = Display::new(cpu)?;
 
     // Run desktop UI
     desktop.run()?;
